e1d65fc91446ca5ece127058065d30b467e88a22
WasabiMayo <hyewon910119@gmail.com>
2016-02-29 18:07:07 -0500

    Initial commit

---
+++ NotificationHW/app/src/main/java/com/example/wasabi/notificationhw/ClaireActivity.java
@@ -0,0 +1,13 @@
+package com.example.wasabi.notificationhw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claire);
+    }
+}
