8f18e3424627b28bbbcd57abbdc185ea2c3aa3e1
roberrera <roberterrera@gmail.com>
2016-02-29 20:42:18 -0500

    Add buttons and onClick listeners

--- app/src/main/java/com/roberterrera/notifications_hw/MainActivity.java
+++ app/src/main/java/com/roberterrera/notifications_hw/MainActivity.java
@@ -2,12 +2,44 @@ package com.roberterrera.notifications_hw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mTestTitle;
+    Button mUSBDebugging;
+    Button mTwitter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTestTitle = (Button) findViewById(R.id.button_testtitle);
+        mUSBDebugging = (Button) findViewById(R.id.button_usbdebugging);
+        mTwitter = (Button) findViewById(R.id.button_twitter);
+
+        mTestTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mUSBDebugging.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
