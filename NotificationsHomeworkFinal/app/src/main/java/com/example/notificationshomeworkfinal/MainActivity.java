f58329cfe485ed54245329b30e19a5962e07dac6
adityosagir <adityo.sagir@gmail.com>
2016-03-01 17:17:55 -0500

    Everything should be merged

--- NotificationsHomeworkFinal/app/src/main/java/com/example/notificationshomeworkfinal/MainActivity.java
+++ NotificationsHomeworkFinal/app/src/main/java/com/example/notificationshomeworkfinal/MainActivity.java
@@ -22,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBuilder = new NotificationCompat.Builder(MainActivity.this);
-        mSeamlessFab = (FloatingActionButton)findViewById(R.id.seamless_fab);
+        mSeamlessFab = (FloatingActionButton) findViewById(R.id.seamless_fab);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mBuilder.setSmallIcon(android.R.drawable.ic_menu_info_details);
-                mBuilder.setContentTitle("Clear Notifacations");
+                mBuilder.setContentTitle("Clear Notifications");
 
 
                 Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
@@ -58,11 +58,11 @@ public class MainActivity extends AppCompatActivity {
                 mBuilder.setContentIntent(pendingIntent);
                 Notification notification = mBuilder.build();
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                notificationManager.notify(1, notification);
+                notificationManager.notify(2, notification);
             }
         });
 
-        mDinnerWithMikeFab = (FloatingActionButton)findViewById(R.id.dinner_with_mike_fab);
+        mDinnerWithMikeFab = (FloatingActionButton) findViewById(R.id.dinner_with_mike_fab);
         mDinnerWithMikeFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,35 +75,85 @@ public class MainActivity extends AppCompatActivity {
                 mBuilder.addAction(R.drawable.ic_archive_grey_24dp, "ARCHIVE", pendingIntent);
                 mBuilder.addAction(R.drawable.ic_reply_black_24dp, "REPLY", pendingIntent);
 
-
                 mBuilder.setContentIntent(pendingIntent);
                 Notification notification = mBuilder.build();
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                notificationManager.notify(1, notification);
+                notificationManager.notify(3, notification);
+
+            }
+        });
+
+        FloatingActionButton fab0 = (FloatingActionButton) findViewById(R.id.fab0);
+        fab0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+
+                builder.setSmallIcon(android.R.drawable.ic_lock_idle_lock);
+                builder.setContentTitle("This is a test title");
+                builder.setContentText("This is some text used for the content of this notification");
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, (int) System.currentTimeMillis(), intent, 0);
+
+                builder.setContentIntent(pendingIntent);
+
+                Notification notification = builder.build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(4, notification);
+
+            }
+        });
+
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+
+                builder.setSmallIcon(android.R.drawable.ic_menu_gallery);
+                builder.setContentTitle("Instagram");
+                builder.setContentText("Go behind the scenes at The Oscars");
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, (int) System.currentTimeMillis(), intent, 0);
+
+                builder.setContentIntent(pendingIntent);
+
+                Notification notification = builder.build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(5, notification);
+
+            }
+        });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+
+                builder.setSmallIcon(android.R.drawable.ic_menu_compass);
+                builder.setContentTitle("USB debugging connected");
+                builder.setContentText("Touch to disable USB debugging.");
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, (int) System.currentTimeMillis(), intent, 0);
+
+                builder.setContentIntent(pendingIntent);
+
+                Notification notification = builder.build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(6, notification);
 
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
